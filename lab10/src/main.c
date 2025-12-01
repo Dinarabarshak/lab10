785767ffec4cebafd9713dcf5f2d124d66a26729
Dinarabarshak <Dinara.Barshakayeva@cs.khpi.edu.ua>
2025-12-01 10:33:19 +0100

    Update main.c

--- lab10/src/main.c
+++ lab10/src/main.c
@@ -1,35 +1,36 @@
-#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib.h"
 
 /*
-  Лабораторна робота №10
-  Завдання №5
-  Тема: Масиви та обчислення значень
-  Автор: Дінара Баршак
-  Дата: 2025
-*/
+ * Лабораторна робота №10
+ * Завдання: Масиви та показчики
+ * Автор: Дінара Баршакаєва
+ * Рік: 2025
+ */
 
-int main() {
-    int n;
-    printf("Введіть кількість елементів масиву: ");
-    scanf("%d", &n);
+int main(void) {
+    enum { LENGTH = 8 };
+    const size_t length = LENGTH;
+    const int predefined[LENGTH] = {4, -2, 7, 3, 0, -5, 9, 6};
 
-    float arr[n];
-    float sum = 0;
-
-    printf("Введіть елементи масиву:\n");
-
-    for (int i = 0; i < n; i++) {
-        printf("arr[%d] = ", i);
-        scanf("%f", &arr[i]);
+    int *data = malloc(length * sizeof(int));
+    if (data == NULL) {
+        return 1;
     }
 
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > 0) {
-            sum += arr[i];
-        }
+    for (size_t i = 0; i < length; ++i) {
+        *(data + i) = *(predefined + i);
     }
 
-    printf("Сума додатних елементів = %.3f\n", sum);
+    int *between = NULL;
+    size_t between_length = 0;
+    long long sum_between = 0;
 
-    return 0;
+    int status = build_between_min_max(data, length, &between, &between_length, &sum_between);
+
+    free(data);
+    free(between);
+
+    return status == 0 ? 0 : 1;
 }
